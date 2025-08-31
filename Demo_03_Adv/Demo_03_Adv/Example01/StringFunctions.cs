f6eec151ba99672a4be7edbb764ce05c01817ac2
Your Name <Nada Elbadry nadaelbadry1012@gmail.com>
2025-08-31 23:27:04 +0300

    Example 01

---
+++ Demo_03_Adv/Demo_03_Adv/Example01/StringFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_03_Adv.Example01
+{
+    internal static class StringFunctions
+    {
+        public static int GetCountOfUpperCaseChar(string str)
+        {
+            int count = 0;
+            if (!string.IsNullOrWhiteSpace(str))
+                for (int i = 0; i < str.Length; i++)
+                    if (char.IsUpper(str[i]))
+                        count++;
+            return count;
+        }
+
+        public static int GetCountOfLowerCaseChar(string str)
+        {
+            int count = 0;
+            if (!string.IsNullOrWhiteSpace(str))
+                for (int i = 0; i < str.Length; i++)
+                    if (char.IsLower(str[i]))
+                        count++;
+            return count;
+        }
+    }
+}
--- Demo_03_Adv/Demo_03_Adv/Program.cs
+++ Demo_03_Adv/Demo_03_Adv/Program.cs
@@ -2,9 +2,43 @@
 {
     internal class Program
     {
+        //// Step 0 : Defining a Delegate
+        //public delegate int StringFuncDelegate(string s);
+        //// New Delegate [Class]
+        //// Reference From This Delegate Can Refer To 
+        //// One Function Or More [Pointer To Function]
+        //// This Functions May Be Static Function Or Non Static Function 
+        //// But These Function Must Be With The Same Signature Of Delegate
+        //// Regardless Function Name and Access Modifier 
         static void Main(string[] args)
         {
-           
+            #region Example 01
+            //// Step 01 : Declare reference From Delegate 
+            //Func<string, int>? stringFunc;
+
+            //// Step 02 : Initialize Reference From Delegate[Pointer To Function]
+
+            //stringFunc = StringFunctions.GetCountOfUpperCaseChar; // Syntax Sugar
+
+            //stringFunc += StringFunctions.GetCountOfLowerCaseChar;
+            //// Add New Reference For Function To Reference From Delegate 
+
+            //stringFunc -= StringFunctions.GetCountOfUpperCaseChar;
+            //// Remove Reference Of GetCountOfUpperCaseChar() Method
+
+            //// Step 03 : Use Delegate Reference [Invoke Method]
+
+            //int Result = stringFunc?.Invoke("RoutE") ?? 0;
+
+            ////Result = stringFunc("RoutE");
+            //if (stringFunc != null)
+            //	Result = stringFunc("RoutE");
+            //else
+            //	Result = 0;
+
+            //Console.WriteLine(Result); // 2 
+            #endregion
+
         }
     }
 }
