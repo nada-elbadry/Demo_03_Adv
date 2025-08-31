299d584bab4f7d104e5396d02eed271edf51e688
Your Name <Nada Elbadry nadaelbadry1012@gmail.com>
2025-08-31 23:59:48 +0300

    Asssigment03

---
+++ Assigment03_Adv/Assigment03_Adv/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment03_Adv
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+        public Book(string title, string author, string isbn, DateTime pubDate)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            PublicationDate = pubDate;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Author} - {ISBN} - {PublicationDate:yyyy-MM-dd}";
+        }
+    }
+}
---
+++ Assigment03_Adv/Assigment03_Adv/LibraryEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment03_Adv
+{
+    internal static class LibraryEngine
+    {
+        // (a) Using user defined delegate
+        //public static void ProcessBooks(List<Book> books, BookFunctions.BookDelegate fPtr)
+        //{
+        //    foreach (var book in books)
+        //    {
+        //        Console.WriteLine(fPtr(book));
+        //    }
+        //}
+
+        // (b) Using BCL delegates (Func)
+        public static void ProcessBooks(List<Book> books, Func<Book, string> fPtr)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(fPtr(book));
+            }
+        }
+    }
+}
